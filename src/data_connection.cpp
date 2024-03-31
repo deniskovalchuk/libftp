838f562bf1c496670b85223d4bec0302db6df4c1
deniskovalchuk <denis.kovalchuk.main@gmail.com>
2024-03-31 19:03:32 +0300

    core: use std::array::size() method

--- src/data_connection.cpp
+++ src/data_connection.cpp
@@ -187,7 +187,7 @@ void data_connection::recv(output_stream & stream, transfer_callback * transfer_
     std::array<char, 8192> buf;
     std::size_t size;
 
-    while ((size = socket_ptr_->read_some(buf.data(), buf.max_size(), ec)) > 0)
+    while ((size = socket_ptr_->read_some(buf.data(), buf.size(), ec)) > 0)
     {
         stream.write(buf.data(), size);
 
