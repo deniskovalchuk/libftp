5da31807dd0c1457d495fc9b84344fccb72dc470
deniskovalchuk <denis.kovalchuk.main@gmail.com>
2024-11-04 13:48:53 +0300

    example: add missing include

--- example/file_transfer_progress.cpp
+++ example/file_transfer_progress.cpp
@@ -25,6 +25,7 @@
 #include <iostream>
 #include <sstream>
 #include <ftp/client.hpp>
+#include <ftp/transfer_callback.hpp>
 #include <ftp/stream/istream_adapter.hpp>
 #include "reply_handlers.hpp"
 
